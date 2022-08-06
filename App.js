41e6a0ff54c50b3c248b2ecc4bede81df1e01335
Eldar <peacemakerkg@gmail.com>
2022-08-06 08:39:04 -0700

    Updated packages

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import Main from "./screens/MainComponent";
+import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
@@ -6,15 +6,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import Loading from './components/LoadingComponent';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={<Loading />} persistor={persistor}>
-        <NavigationContainer>
-          <Main />
-        </NavigationContainer>
-      </PersistGate>
-    </Provider>
-  )
+    return (
+        <Provider store={store}>
+            <PersistGate loading={<Loading />} persistor={persistor}>
+                <NavigationContainer>
+                    <Main />
+                </NavigationContainer>
+            </PersistGate>
+        </Provider>
+    );
 }
-
-
--- redux/store.js
+++ redux/store.js
@@ -15,11 +15,13 @@ import {
     REGISTER
 } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const config = {
     key: 'root',
     storage: AsyncStorage,
     debug: true
-}
+};
+
 export const store = configureStore({
     reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
